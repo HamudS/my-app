25780591e2f49db01e3022f0bd45e232b294b1fe
HamudS <mohibns2012@gmail.com>
2024-02-29 15:50:50 +0300

    some components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Header } from "./components/Header";
+import Main from "./components/Main";
+import { Nav } from "./components/Nav";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>HomePage</h1>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
+};
